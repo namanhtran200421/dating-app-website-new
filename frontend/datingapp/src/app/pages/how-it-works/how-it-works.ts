a9610297351cc35a17e149f7f0c0254387db3864
Floatingdeadfish <quangvietvu0404@gmail.com>
2026-09-18 03:31:20 +0930

    ready for publish

--- frontend/datingapp/src/app/pages/how-it-works/how-it-works.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-how-it-works-page',
-  templateUrl: './how-it-works.html',
-  styleUrl: './how-it-works.css',
-})
-export class HowItWorksPage {}
